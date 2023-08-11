26ace984631187c7ab430dcd33d0cba2504dc128
Amin <56980571+m-parchami@users.noreply.github.com>
2023-08-11 14:39:00 +0200

    Update awesome_commands.sh

--- awesome_commands.sh
+++ awesome_commands.sh
@@ -2,7 +2,7 @@
 du -a | sed '/.*\.\/.*\/.*/!d' | cut -d/ -f2 | sort | uniq -c | sort -nr
 
 ## Touch the repositories so that they don't get deleted by IST :)
-find ./someDir/ -type f | xargs -0 -P $(nproc) -n 5000 touch
+find ./someDir/ -type f -print0 | xargs -0 -P $(nproc) -n 5000 touch
 # Potentially you can add -maxdepth -mindepth to make the search faster
 
 ## List all the directories without a certain file:
